68b9e6af5c229f8a05c880a8df513367e6ddbb53
kev43 <kev43200@gmail.com>
2017-02-01 14:35:49 +0100

    add struct debut

--- main.c
+++ main.c
@@ -15,9 +15,16 @@ David Vivier et Kévin Varagnat
 
 #define TAILLE 50
 
+typedef struct mots mots ;
+struct mots{
+	char libelle_mot[10];
+	int type_mot;
+}
 
 
 int main() {
+	
+	mots dictionnaire[10]={{"le",0},{"chat",2},{"souris",2},{"mange",3},{"la",0},{"petite",1},{".",5},{"bleu",1},{"dort",3},{"julie",4},};
 
 	return 0;
 }
